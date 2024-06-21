b7b8b990494061b9a77837c7138ee5eb3e6a213f
DmitryCo <dm3ypost@gmail.com>
2024-06-22 00:59:03 +0300

    Add case Prime.startPrimeGame() in switch to method selectGames()

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -4,13 +4,14 @@ import hexlet.code.games.Even;
 import hexlet.code.games.Calc;
 import hexlet.code.games.GCD;
 import hexlet.code.games.Progression;
+import hexlet.code.games.Prime;
 
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
         System.out.println("Please enter the game number and press Enter.");
-        System.out.println("1 - Greet\n2 - Even\n3 - Calc\n4 - GCD\n5 - Progression\n0 - Exit");
+        System.out.println("1 - Greet\n2 - Even\n3 - Calc\n4 - GCD\n5 - Progression\n6 - Prime\n0 - Exit");
 
         Scanner scanner = new Scanner(System.in);
         int userChoice;
@@ -35,6 +36,9 @@ public class App {
                 break;
             case 5:
                 Progression.startProgressionGame();
+                break;
+            case 6:
+                Prime.startPrimeGame();
                 break;
             case 0:
                 System.out.println("0 - Exit");
