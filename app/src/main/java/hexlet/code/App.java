e1445e36885358e6baad287ad14421a7a4be5fc2
DmitryCo <dm3ypost@gmail.com>
2024-06-25 00:41:48 +0300

    Fix magic number in switch in the App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -14,33 +14,33 @@ public class App {
         System.out.println("1 - Greet\n2 - Even\n3 - Calc\n4 - GCD\n5 - Progression\n6 - Prime\n0 - Exit");
 
         Scanner scanner = new Scanner(System.in);
-        int userChoice;
+        String userChoice;
         System.out.print("Your choice: ");
-        userChoice = Integer.parseInt(scanner.next());
+        userChoice = scanner.next();
         selectGames(userChoice);
     }
 
-    static void selectGames(int userChoice) {
+    static void selectGames(String userChoice) {
         switch (userChoice) {
-            case 1:
+            case "1":
                 Cli.getUserName();
                 break;
-            case 2:
+            case "2":
                 Even.startEvenGame();
                 break;
-            case 3:
+            case "3":
                 Calc.startCalcGame();
                 break;
-            case 4:
+            case "4":
                 GCD.startGCDGame();
                 break;
-            case 5:
+            case "5":
                 Progression.startProgressionGame();
                 break;
-            case 6:
+            case "6":
                 Prime.startPrimeGame();
                 break;
-            case 0:
+            case "0":
                 System.out.println("0 - Exit");
                 break;
             default:
