90f29fd159a590033d24dcf745d41d9d637f89e0
DmitryCo <dm3ypost@gmail.com>
2026-03-14 20:33:04 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,9 +1,8 @@
 plugins {
-    id("java")
-    id("com.github.ben-manes.versions") version "0.52.0"
-    id("org.sonarqube") version "6.3.1.5724"
     application
     checkstyle
+    id("com.github.ben-manes.versions") version "0.52.0"
+    id("org.sonarqube") version "6.3.1.5724"
 }
 
 application {
@@ -18,16 +17,10 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
 tasks.getByName("run", JavaExec::class) {
 	standardInput = System.`in`
-}
-
-tasks.test {
-    useJUnitPlatform()
 }
 
 sonar {
